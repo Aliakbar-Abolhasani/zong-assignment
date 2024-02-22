8d060c7a838bcc5a7278c73f9b38caab94eee0c6
Atheris <accpcshd@gmail.com>
2024-02-23 01:50:43 +0330

    Added CameraBillboard utility script

---
+++ Assets/_Game/Scripts/Utils/CameraBillboard.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace ZongDemo.Utils
+{
+    public class CameraBillboard : MonoBehaviour
+    {
+        [SerializeField] private bool _yAxisOnly;
+
+        private Camera _camera;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+        }
+
+        private void LateUpdate()
+        {
+            var forward = _camera.transform.forward;
+            if (_yAxisOnly)
+            {
+                forward.y = 0;
+            }
+
+            transform.forward = forward;
+        }
+    }
+}
