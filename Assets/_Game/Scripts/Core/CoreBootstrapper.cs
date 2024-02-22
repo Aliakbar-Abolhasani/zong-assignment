6076c9615c44a96348cfffc7715c1a56211b80f5
Atheris <accpcshd@gmail.com>
2024-02-23 01:51:51 +0330

    Added CoreBootstrapper

---
+++ Assets/_Game/Scripts/Core/CoreBootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using ZongDemo.Core.Inventory;
+using ZongDemo.Core.PlayerStats;
+using ZongDemo.Core.UI.Popups;
+
+namespace ZongDemo.Core
+{
+    public class CoreBootstrapper : MonoBehaviour
+    {
+        private void Awake()
+        {
+            ServiceLocator.Instance
+                .Register<IInventoryService>(new InMemoryInventoryService())
+                .Register<IPlayerStatsService>(new InMemoryPlayerStatsService())
+                .Register<IPopupService>(new PopupService());
+        }
+
+        private void OnDestroy()
+        {
+            ServiceLocator.Instance
+                .Unregister<IInventoryService>()
+                .Unregister<IPlayerStatsService>()
+                .Unregister<IPopupService>();
+        }
+    }
+}
